da09a257032e69910231388b7a2f53481669ffdd
Victor Brayner <victor.brayner@hotmail.com>
2021-04-22 13:54:06 -0300

    feat: save new plants

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -5,7 +5,8 @@ import colors from "../styles/colors";
 import Welcome from "../pages/Welcome";
 import UserIdentification from "../pages/UserIdentification";
 import Confirmation from "../pages/Confirmation";
-import PlantSelect from "../pages/PlantSelect";
+import PlantSave from "../pages/PlantSave";
+import AuthRoutes from "./tab.routes";
 
 const StackRoutes = createStackNavigator();
 
@@ -24,7 +25,9 @@ const AppRoutes: React.FC = () => (
       component={UserIdentification}
     />
     <StackRoutes.Screen name="Confirmation" component={Confirmation} />
-    <StackRoutes.Screen name="PlantSelect" component={PlantSelect} />
+    <StackRoutes.Screen name="PlantSelect" component={AuthRoutes} />
+    <StackRoutes.Screen name="PlantSave" component={PlantSave} />
+    <StackRoutes.Screen name="MyPlants" component={AuthRoutes} />
   </StackRoutes.Navigator>
 );
 
